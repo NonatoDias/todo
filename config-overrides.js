82be9499cbb8abb1140d67324752de4a5570b4f3
nonato dias home <nonato94@gmail.com>
2019-07-28 22:50:52 -0300

    custom sw

---
+++ config-overrides.js
@@ -0,0 +1,18 @@
+const {
+	rewireWorkboxInject,
+	defaultInjectConfig
+} = require('react-app-rewire-workbox');
+const path = require('path');
+
+module.exports = function override(config, env) {
+	if (env === 'production') {
+		const workboxConfig = {
+			...defaultInjectConfig,
+			swSrc: path.join(__dirname, 'src/sw', 'custom-sw.js'),
+			swDest: 'service-worker.js'
+		};
+		config = rewireWorkboxInject(workboxConfig)(config, env);
+	}
+
+	return config;
+};
