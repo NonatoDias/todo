25edb9d7436bf499a5be21e4d65e8dd8a38beb89
nonato dias home <nonato94@gmail.com>
2019-07-28 23:11:39 -0300

    base url and first deploy

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './App.scss';
 import { NavBar } from './components';
 import { ToDo } from './container/toDo/ToDo';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
 	return (
 		<div className="App">
-			<NavBar/>
-			<ToDo />
+			<NavBar />
+			<Switch>
+				<Route path="/" exact={true} component={ToDo} />
+			</Switch>
 		</div>
 	);
 }
